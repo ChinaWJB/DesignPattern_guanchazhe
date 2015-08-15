75a872b2367a5d35ba3d9079e69ee47605316238
ChinaWJB <chinawjb@foxmail.com>
2015-08-15 16:32:57 +0800

    headfirst

--- src/com/wjb/observer_impl/CurrentConditionsDisplay.java
+++ src/com/wjb/observer_impl/CurrentConditionsDisplay.java
@@ -17,7 +17,7 @@ public class CurrentConditionsDisplay implements Observer, DisplayElement{
 		weatherData.registerObserver(this);				//为什么要注册？ 因为这个观察者中有update和display方法，
 														//主题（可观察者）要通过update方法给观察者发送通知
 														//只是添加到了数组链表中
-														//要注册的是一个观察者或子类（布告板）
+														//要注册的是一个观察者或子类（布告板）.
 	}
 	
 	public void update (float temperature, float humidity, float pressure){
